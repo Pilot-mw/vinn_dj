80223fc7c1a8a8e083be38d5bd8e6c1b3033c923
Pilot-mw <mtiwajason@gmail.com>
2026-04-22 15:28:10 +0200

    fix admin creation

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Create superuser automatically
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(username='admin').exists() or
+User.objects.create_superuser('admin','admin@example.com','admin12345')" | python manage.py shell
