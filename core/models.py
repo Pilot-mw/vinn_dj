0e5010d65341c800e757d92e5e93135834f3b6e5
Pilot-mw <mtiwajason@gmail.com>
2026-04-23 08:18:27 +0200

    fix cloudinary storage

--- core/models.py
+++ core/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
+class GalleryImage(models.Model):
+    title = models.CharField(max_length=100)
+    image = CloudinaryField('image')
+    
+class Track(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=100)
+    cover_image = CloudinaryField('image', blank=True, null=True)
+    audio = CloudinaryField(resource_type='raw')
+    duration = models.CharField(max_length=10, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 class Mix(models.Model):
     title = models.CharField(max_length=200)
